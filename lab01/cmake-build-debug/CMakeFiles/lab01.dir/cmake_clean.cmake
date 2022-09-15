e95e08b485e03517df6180019ee2443f351b324d
pi-dell <pi@drivecast.tech>
2022-09-15 20:50:52 +0300

    lab01

---
+++ lab01/cmake-build-debug/CMakeFiles/lab01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab01.dir/src/CipherHelper.cpp.o"
+  "liblab01.a"
+  "liblab01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
